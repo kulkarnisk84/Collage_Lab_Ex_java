10e7114d27969eecd20dd566d1ca928129da39b3
kulkarnisk84 <kulkarnisk84@gmail.com>
2018-09-13 21:06:41 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Table
+{
+	public static void main(String [] args)
+	{
+		Scanner T=new Scanner(System.in);
+		System.out.print("Enter Table U wont:");
+		int a = T.nextInt();
+			for(int i=1;i<=10;i++)
+			{
+				int B=i*a;
+				System.out.print(" "+B);
+				
+			}
+		
+		
+	}
+	
+	
+}
